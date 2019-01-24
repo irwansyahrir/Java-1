763db207e721aeaa40f03c37443170f70752e2c6
irwansyahrir <irwan.syahrir@gmail.com>
2019-01-24 22:34:43 +0100

    Add more tests

--- src/test/kotlin/search/BinarySearchKotlinTest.kt
+++ src/test/kotlin/search/BinarySearchKotlinTest.kt
@@ -5,8 +5,27 @@ import org.junit.jupiter.api.Test
 
 class BinarySearchKotlinTest {
     @Test
-    internal fun testBinarySearch() {
-        val array = arrayOf(1, 2, 3, 4, 5)
-        assertEquals(2, BinarySearch.findIndex(array, 3))
+    internal fun testBinarySearchWithNumbers() {
+        val numbers = arrayOf(1, 2, 3, 4, 5)
+        assertEquals(2, BinarySearch.findIndex(numbers, 3))
+        assertEquals(-1, BinarySearch.findIndex(numbers, 8))
+    }
+
+    @Test
+    internal fun binarySearchWithChars() {
+        val characters = arrayOf("a", "b", "c")
+        assertEquals(2, BinarySearch.findIndex(characters, "c"))
+    }
+
+    @Test
+    internal fun binarySearchWithWords() {
+        val words = arrayOf("satu", "dua", "tiga", "empat")
+        assertEquals(1, BinarySearch.findIndex(words, "dua"))
+        assertEquals(-1, BinarySearch.findIndex(words, "dualima"))
+    }
+
+    @Test
+    internal fun binarySearchWithEmptyArray() {
+        assertEquals(-1, BinarySearch.findIndex(emptyArray<String>(), "1"))
     }
 }
